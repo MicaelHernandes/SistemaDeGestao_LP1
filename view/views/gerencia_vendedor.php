5463eb480eb54a8af649752143d619038ed86f44
Micael Hernandes <micaelmacedo100@outlook.com>
2023-06-04 11:30:03 -0300

    começando a criar o painel de gerencimento de vendas/vendedor

---
+++ view/views/gerencia_vendedor.php
@@ -0,0 +1,13 @@
+<body>
+    <div class="row h-80 w-80 d-flex mt-4 mb-4 text-center">
+        <h2>Bem vindo ao painel de gerenciamento de vendas/vendedor!</h2>
+    </div>
+    <div class="row h-80 w-80 mx-auto flex-row mt-4 text-center">
+        <div class="col">
+            aa
+        </div>
+        <div class="col">
+            aa
+        </div>
+    </div>
+</body>
